7ca44a674f2363010a80a3037bcbdd3737e0943a
belvip <pouadjeubelvi@gmail.com>
2025-05-10 11:50:23 +0200

    feat: setting up projet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import toast, { Toaster } from "react-hot-toast";
+import { motion } from "framer-motion";
 
 function App() {
+  const notify = () => toast("Here is your toast.");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={notify}>Make me a toast</button>
+        <Toaster />
+      </div>
+
+      <motion.div
+        className="motion-div" 
+        animate={{ x: 100, opacity: 1 }}
+        initial={{ x: 0, opacity: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <h1>Welcome to the login page</h1>
+      </motion.div>
+
     </div>
   );
 }
